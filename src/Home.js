58c9a490994cee500841029c0fa17daed1e8f40a
Alex Buzea <alexbuzea@ymail.com>
2023-08-04 12:26:32 +0100

    hosted json server outside github in order to render the page on github pages

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,33 @@
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
+// const Home = () => {
+//   const {
+//     data: blogs,
+//     isPending,
+//     error,
+//   } = useFetch("http://localhost:8000/blogs"); //destructure assignemnt to extract 3 variables which will hold corresponding data
+
+//   return (
+//     <div className="home">
+//       {/* if any err, show err message on page */}
+//       {error && <div>{error}</div>}
+//       {isPending && <div> Loading... </div>}{" "}
+//       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+//     </div>
+//   );
+// };
+
+//
+
 const Home = () => {
   const {
     data: blogs,
     isPending,
     error,
-  } = useFetch("http://localhost:8000/blogs"); //destructure assignemnt to extract 3 variables which will hold corresponding data
+  } = useFetch(
+    "https://my-json-server.typicode.com/AlexBuzea/react_intro_project/blogs"
+  ); //destructure assignemnt to extract 3 variables which will hold corresponding data
 
   return (
     <div className="home">
